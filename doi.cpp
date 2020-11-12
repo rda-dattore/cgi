dcc9fb6c56b7321dc50bc1312e71f859c8a44fb0
rda-dattore <dattore@ucar.edu>
2020-11-12 14:43:21 -0700

    refactored

--- doi.cpp
+++ doi.cpp
@@ -16,11 +16,7 @@
 std::string myerror="";
 std::string mywarning="";
 
-const std::string NCAR_PREFIX="10.5065/";
-
-std::string doi,type,dsnum;
-
-void parseQueryString()
+void parse_query_string(std::string& doi)
 {
   QueryString queryString(QueryString::GET);
 
@@ -28,12 +24,12 @@ void parseQueryString()
   if (doi.length() == 0) {
     web_error2("missing DOI","400 Bad Request");
   }
-  if (!std::regex_search(doi,std::regex("^"+NCAR_PREFIX))) {
+  if (!std::regex_search(doi,std::regex("^10.5065/"))) {
     web_error2("invalid DOI","400 Bad Request");
   }
 }
 
-void printTitle()
+void print_web_page_title()
 {
   std::cout << "Content-type: text/html" << std::endl << std::endl;
   std::cout << "<span class=\"fs24px bold\">RDA DOI Resolver</span>" << std::endl;
@@ -41,39 +37,45 @@ void printTitle()
 
 int main(int argc,char **argv)
 {
-  MySQL::LocalQuery query,query2;
-  MySQL::Row row,row2;
-  XMLDocument xdoc;
-
 // read the cgi-bin configuration
   webutils::cgi::Directives directives;
   if (!webutils::cgi::read_config(directives)) {
     web_error2("unable to read configuration","500 Internal Server Error");
   }
 
-  parseQueryString();
+// parse the query string
+  std::string doi;
+  parse_query_string(doi);
+
+// connect to the database
   MySQL::Server server(directives.database_server,directives.rdadb_username,directives.rdadb_password,"dssdb");
   if (!server) {
     web_error2("unable to connect to the database","500 Internal Server Error");
   }
-  query.set("select d.dsid,d.status,g.logname,g.fstname,g.lstname from dsvrsn as d left join dsowner as o on o.dsid = d.dsid left join dssgrp as g on g.logname = o.specialist where d.doi = '"+doi+"' order by find_in_set(status,'A,H')");
-  if (query.submit(server) < 0) {
+
+// query the version table for the dataset
+  MySQL::LocalQuery version_query("select v.dsid,v.status,g.logname,g.fstname,g.lstname from dsvrsn as v left join dsowner as o on o.dsid = v.dsid left join dssgrp as g on g.logname = o.specialist where v.doi = '"+doi+"' order by find_in_set(status,'A,H')");
+  if (version_query.submit(server) < 0) {
     web_error2("database query error","500 Internal Server Error");
   }
-  if (query.num_rows() == 0) {
+  if (version_query.num_rows() == 0) {
     web_error2("DOI not found","400 Bad Request");
   }
   else {
-    if (!query.fetch_row(row) != 0) {
+    MySQL::Row version_query_row;
+    if (!version_query.fetch_row(version_query_row) != 0) {
 	web_error2("unable to connect to the database","500 Internal Server Error");
     }
-    if (row[1] == "A") {
-	query2.set("title,pub_date","search.datasets","dsid = '"+row[0].substr(2)+"'");
-	if (query2.submit(server) < 0) {
+    auto dsnum=version_query_row["v.dsid"];
+    auto ds_status=version_query_row["v.status"];
+    if (ds_status == "A") {
+	MySQL::LocalQuery dataset_query("title,pub_date","search.datasets","dsid = '"+dsnum.substr(2)+"'");
+	if (dataset_query.submit(server) < 0) {
 	  web_error2("database query error","500 Internal Server Error");
 	}
-	query2.fetch_row(row2);
-	printTitle();
+	MySQL::Row dataset_query_row;
+	dataset_query.fetch_row(dataset_query_row);
+	print_web_page_title();
 	std::cout << "<script id=\"citation_script\" language=\"javascript\">" << std::endl;
 	std::cout << "function changeCitation() {" << std::endl;
 	std::cout << "  if (document.citation)" << std::endl;
@@ -81,46 +83,47 @@ int main(int argc,char **argv)
 	std::cout << "  else" << std::endl;
 	std::cout << "    var style='esip';" << std::endl;
 	std::cout << "  document.getElementById(\"citation\").innerHTML=\"Loading the citation...<br /><br />\";" << std::endl;
-	std::cout << "  getContent('citation','/cgi-bin/datasets/citation?dsnum=" << row[0] << "&style='+style);" << std::endl;
+	std::cout << "  getContent('citation','/cgi-bin/datasets/citation?dsnum=" << dsnum << "&style='+style);" << std::endl;
 	std::cout << "}" << std::endl;
 	std::cout << "registerAjaxCallback('changeCitation');" << std::endl;
 	std::cout << "</script>" << std::endl;
 	std::cout << "<ul>" << std::endl;
 	std::cout << "<p><b>DOI:</b>&nbsp;&nbsp;" << doi << "</p>" << std::endl;
-	std::cout << "<p><b>RDA Dataset ID:</b>&nbsp;&nbsp;" << row[0] << "</p>" << std::endl;
-	std::cout << "<p><b>RDA Dataset Title:</b>&nbsp;&nbsp;<span class=\"underline\">" << row2[0] << "</span></p>" << std::endl;
-	std::cout << "<p><b>URL:</b>&nbsp;&nbsp;<a target=\"_blank_doi\" href=\"/datasets/" << row[0] << "/\">http://rda.ucar.edu/datasets/" << row[0] << "/</a>&nbsp;<img src=\"/images/newwin.gif\" title=\"opens in a new window or tab\" /></p>" << std::endl;
-	if (xdoc.open("/usr/local/www/server_root/web/datasets/"+row[0]+"/metadata/dsOverview.xml")) { {
-	  std::cout << "<b>How to Cite This Dataset:</b><table cellspacing=\"5\" cellpadding=\"3\" border=\"0\"><tr valign=\"top\"><td><div style=\"background-color: #2a70ae; color: white; width: 40px; padding: 1px; margin-top: 3px; font-size: 16px; font-weight: bold; border-radius: 5px 5px 5px 5px; text-align: center; cursor: pointer\" onClick=\"javascript:location='/cgi-bin/datasets/citation?dsnum=" << row[0] << "&style=ris'\" title=\"download citation in RIS format\">RIS</div></td><td><div id=\"citation\"></div></td></tr></table>" << std::endl;
+	std::cout << "<p><b>RDA Dataset ID:</b>&nbsp;&nbsp;" << dsnum << "</p>" << std::endl;
+	std::cout << "<p><b>RDA Dataset Title:</b>&nbsp;&nbsp;<span class=\"underline\">" << dataset_query_row[0] << "</span></p>" << std::endl;
+	std::cout << "<p><b>URL:</b>&nbsp;&nbsp;<a target=\"_blank_doi\" href=\"/datasets/" << dsnum << "/\">http://rda.ucar.edu/datasets/" << dsnum << "/</a>&nbsp;<img src=\"/images/newwin.gif\" title=\"opens in a new window or tab\" /></p>" << std::endl;
+	XMLDocument ds_overview;
+	if (ds_overview.open("/usr/local/www/server_root/web/datasets/"+dsnum+"/metadata/dsOverview.xml")) { {
+	  std::cout << "<b>How to Cite This Dataset:</b><table cellspacing=\"5\" cellpadding=\"3\" border=\"0\"><tr valign=\"top\"><td><div style=\"background-color: #2a70ae; color: white; width: 40px; padding: 1px; margin-top: 3px; font-size: 16px; font-weight: bold; border-radius: 5px 5px 5px 5px; text-align: center; cursor: pointer\" onClick=\"javascript:location='/cgi-bin/datasets/citation?dsnum=" << dsnum << "&style=ris'\" title=\"download citation in RIS format\">RIS</div></td><td><div id=\"citation\"></div></td></tr></table>" << std::endl;
 	  }
-	  xdoc.close();
+	  ds_overview.close();
 	}
 	std::cout << "</ul>" << std::endl;
     }
-    else if (row[1] == "H") {
-	query2.set("doi","dsvrsn","dsid = '"+row[0]+"' and status = 'A'");
-	if (query2.submit(server) < 0) {
+    else if (ds_status == "H") {
+	MySQL::LocalQuery doi_query("doi","dsvrsn","dsid = '"+dsnum+"' and status = 'A'");
+	if (doi_query.submit(server) < 0) {
 	  web_error2("database query error","500 Internal Server Error");
 	}
-	if (query2.num_rows() == 0) {
+	if (doi_query.num_rows() == 0) {
 // terminated DOI
-	  printTitle();
+	  print_web_page_title();
 	}
-	else if (query2.num_rows() == 1) {
+	else if (doi_query.num_rows() == 1) {
 // superseded DOI
-	  printTitle();
-	  std::cout << "<p><img src=\"/images/alert.gif\" />&nbsp;The DOI that you provided: " << doi << " has been superseded by a new version of the data.  Your options are:<ul><li>Go to the <a href=\"/datasets/"+row[0]+"/\">new version</a> of the data</li><li>Contact <a href=\"mailto:"+row[2]+"@ucar.edu?subject=DOI "+doi+"\">"+row[3]+" "+row[4]+"</a> to find out how you can get access to the superseded data";
-	  if (query.num_rows() > 1) {
+	  print_web_page_title();
+	  std::cout << "<p><img src=\"/images/alert.gif\" />&nbsp;The DOI that you provided: " << doi << " has been superseded by a new version of the data.  Your options are:<ul><li>Go to the <a href=\"/datasets/"+dsnum+"/\">new version</a> of the data</li><li>Contact <a href=\"mailto:"+version_query_row["g.logname"]+"@ucar.edu?subject=DOI "+doi+"\">"+version_query_row["g.fstname"]+" "+version_query_row["g.lstname"]+"</a> to find out how you can get access to the superseded data";
+	  if (version_query.num_rows() > 1) {
 	    std::cout << "<ul>Note:  Multiple versions of superseded data exist.  If you know the date that the data were accessed, it will help us get you the exact files associated with this DOI.</ul>";
 	  }
 	  std::cout << "</li></p>" << std::endl;
 	}
-	else
-// SEND EMAIL
+	else {
 	  web_error2("database error","500 Internal Server Error");
+	}
     }
-    else
-// SEND EMAIL
+    else {
 	web_error2("database error","500 Internal Server Error");
+    }
   }
 }
